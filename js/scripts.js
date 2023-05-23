47426ce821fa88d58160229961c269170151979f
Nurali Davlatov <nur3.dav.97@gmail.com>
2023-05-23 17:09:42 +0500

    mobile

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,59 @@
 
+$('#menuBtn').on('click', function () {
+    $('#menuBtn, header, body').toggleClass('active');
+});
 
-if ($('div').hasClass('tlc-img')) {
-    $('.tlc-slider').slick({
-        infinite: false,
-        speed: 300,
-        arrows: false,
-        dots: false,
-        swipeToSlide: true,
-        slidesToShow: 1,
-        asNavFor: '.tlc-nav',
-        touchThreshold: 9,
-        fade: true,
-        responsive: [
-            {
-                breakpoint: 700,
-                settings: {
-                    fade: false,
-                    infinite: true,
-                }
-            }
-        ]
+$('.slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 3,
+    dots: false,
+    speed: 300,
+    variableWidth: true,
+    centerMode: true,
+});
+
+$('.filter__btn').on('click', function () {
+    $('.filter__btn').not($(this)).removeClass('active')
+    $(this).addClass('active');
+});
+
+function hideModals() {
+    $('.modal').fadeOut();
+    $('.modal, body, [data-modal]').removeClass('active');
+};
+$(function () {
+    function showModal(id) {
+        if ($(id).hasClass('active')) {
+            $(id).fadeOut(300)
+            $(id).removeClass('active');
+            $('body').removeClass('active');
+        } else {
+            $(id).addClass('active')
+            $('body').addClass('active');
+            $(id).fadeIn(300);
+        }
+    }
+
+    $('[data-modal]').on('click', function (e) {
+        e.preventDefault();
+        $(this).toggleClass('active')
+        showModal('#' + $(this).attr("data-modal"));
     });
-    $('.tlc-nav').slick({
-        vertical: true,
-        infinite: false,
-        draggable: true,
-        dots: false,
-        focusOnSelect: true,
-        centerMode: true,
-        verticalSwiping: true,
-        arrows: true,
-        slidesToShow: 6,
-        slidesToScroll: 6,
-        asNavFor: '.tlc-slider',
-        responsive: [
-            {
-                breakpoint: 1200,
-                settings: {
-                    slidesToShow: 5,
-                    slidesToScroll: 5,
-                }
-            },
-            {
-                breakpoint: 700,
-                settings: "unslick"
-            }
-        ]
+
+    $('.modal-close').on('click', () => {
+        hideModals();
     });
-}
+
+    $(document).on('click', function (e) {
+        if (!(
+            ($(e.target).parents('.modal-content').length) ||
+            ($(e.target).parents('.nav').length) ||
+            ($(e.target).hasClass('btn')) ||
+            ($(e.target).hasClass('btn-menu')) ||
+            ($(e.target).hasClass('modal-content'))
+        )) {
+            hideModals();
+        }
+    });
+});
